1956e1516cf7652cfa4a2fb20669352e981457ea
govindad <govinda@govindadasu.com>
2017-03-05 17:12:53 -0800

    adding hit_id to track

--- taskapp/admin.py
+++ taskapp/admin.py
@@ -3,6 +3,6 @@ from models import *
 
 # Register your models here.
 class TaskAdmin(admin.ModelAdmin):
-    list_display = ('created', 'last_modified', 'worker_name', 'worker_id', 'requester_name', 'requester_id', 'title', 'reward', 'status', 'feedback')
+    list_display = ('created', 'last_modified', 'worker_name', 'worker_id', 'requester_name', 'requester_id', 'hit_id', 'title', 'reward', 'status', 'feedback')
 
 admin.site.register(Task, TaskAdmin)
--- taskapp/models.py
+++ taskapp/models.py
@@ -6,13 +6,14 @@ import datetime
 
 # Create your models here.
 
-class Task(models.Model):
+class Task(models.Model): # using hit_id+worker_id is the unique identifier
     created = models.DateTimeField(default=datetime.datetime.utcnow, blank=True, null=True)
     last_modified = models.DateTimeField(default=datetime.datetime.utcnow, blank=True, null=True)
     worker_name = models.TextField(default='', blank=True, null=True)
     worker_id = models.TextField(default='', blank=True, null=True)
     requester_name = models.TextField(default='', blank=True, null=True)
     requester_id = models.TextField(default='', blank=True, null=True)
+    hit_id = models.TextField(default='', blank=True)
     title = models.TextField(default='', blank=True, null=True)
     reward = models.TextField(default='', blank=True, null=True)
     status = models.TextField(default='', blank=True, null=True)
