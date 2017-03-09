46aefa4ba078c053e2be4dae8e692763a2230d2d
govindad <govinda@govindadasu.com>
2017-03-09 01:08:34 -0800

    wrote code for generating random samples, have to print them and then insert them into db

--- generate_fake_worker_data.py
+++ generate_fake_worker_data.py
@@ -1,6 +1,11 @@
-import requests
+#import requests
+import calendar
+import time
+import string
+from random import *
 
 NUM_SAMPLES = 1
+BASE_URL = 'http://srbkd-env.us-west-2.elasticbeanstalk.com/'
 
 worker_opts = {'1': 'Ali Alkhatib', '2': 'Junwon Park', '3': 'Katharina Lix', '4': 'Jan Peters', '5': 'Melissa Valentine', '6': 'Niloufar Salehi', '7': 'Spam Girl', '8': 'Shirish Goyal', '9': 'Ranjay Krishna', '10': 'Sherry Ruan'} # {'worker_id': 'worker_name'}
 
@@ -14,6 +19,22 @@ category_opts = ['Sentiment', 'Survey', 'Image Tag', 'Transcription', 'Classific
 
 status_opts = ['Approved - Pending Payment', 'Approved - Paid', 'Rejected', 'Pending Approval']
 
-feedback = ['good', 'bad', 'very good']
+feedback_opts = ['good', 'bad', 'very good']
 
 for s in NUM_SAMPLES:
+    worker_id = random.choice(worker_opts.keys())
+    worker_name = worker_opts[worker_id]
+    requester_id = random.choice(requester_opts.keys())
+    requester_name = requester_opts[requester_id]
+    hit_id = str(int(time.time()*1000000)) + ''.join(choice(string.ascii_uppercase + string.digits) for _ in range(14)) # should be unique -- neg prob of being same
+    category_opts = random.choice(category_opts)
+    reward = "${:.2f}".format(round(random.random(), 2))
+    status = random.choice(status_opts)
+    feedback = random.choice(feedback_opts)
+    '''
+    r = requests.post(BASE_URL + 'task/', data={
+        'number': 12524, 
+        'type': 'issue', 
+        'action': 'show'
+    })
+    '''
