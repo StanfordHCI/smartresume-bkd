3b6f50d4218a1f98c3204dcb6e2385a637e5c57e
govindad <govinda@govindadasu.com>
2017-03-27 02:40:20 -0700

    added models, need to migrate/test witht them

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # srbkd
 backend for smart resume chrome extension
 
+http://srbkd-env.us-west-2.elasticbeanstalk.com/
+
 to deploy:
 - in taskproj
 - zip -r #.zip . && mv #.zip ..
--- taskapp/models.py
+++ taskapp/models.py
@@ -24,3 +24,22 @@ class Task(models.Model): # using hit_id+worker_id is the unique identifier
 
     def __str__(self):
         return self.text
+
+class Worker(models.Model): 
+    created = models.DateTimeField(default=datetime.datetime.utcnow, blank=True, null=True)
+    last_modified = models.DateTimeField(default=datetime.datetime.utcnow, blank=True, null=True)
+    worker_name = models.TextField(default='', blank=True, null=True)
+    worker_id = models.TextField(default='', blank=True, null=True)
+
+class Guild(models.Model): 
+    created = models.DateTimeField(default=datetime.datetime.utcnow, blank=True, null=True)
+    last_modified = models.DateTimeField(default=datetime.datetime.utcnow, blank=True, null=True)
+    guild_name = models.TextField(default='', blank=True, null=True)
+    guild_creator_id = models.TextField(default='', blank=True, null=True) 
+    guild_creator_type = models.BooleanField(default=False) # false for requestor, true for worker
+
+class GuildWorkerMap(models.Model):
+    created = models.DateTimeField(default=datetime.datetime.utcnow, blank=True, null=True)
+    last_modified = models.DateTimeField(default=datetime.datetime.utcnow, blank=True, null=True)     
+    worker = models.ForeignKey('Worker', blank=True, null=True, related_name='guildworkermap')
+    guild = models.ForeignKey('Worker', blank=True, null=True, related_name='guildworkermap')
